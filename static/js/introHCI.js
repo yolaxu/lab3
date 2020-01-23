8a2c97e157a5d9c45a855c8889e237732fbacaf6
Eryue Xu <eryuexu@EryuedeMacBook-Air.local>
2020-01-23 10:18:27 -0800

    lab3updated

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,35 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("name is changed");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
+	$("a.thumbnail").click(projectClick);
+/*
+	$('.project').click(listenerFunction);
+	$('.project').append('<h1>jQuery</h1>');
+	$('.project').hide();*/
 	// example: $("#div-id").click(functionToCall);
 }
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+    var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    $('.project-description').fadeOut()
+}
+
+
+
+/*function projectClick(e){
+	console.log('Project clikced');
+	e.preventDefault();
+	$(this).css('background-color','green')
+}*/
